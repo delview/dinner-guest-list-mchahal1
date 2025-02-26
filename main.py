f8c076058537692893b35bdf061a2ff778eb5b23
mchahal1 <manveerc077@deltalearns.ca>
2025-02-26 18:19:13 +0000

    added errorhandiling function and loop for removing a person

--- main.py
+++ main.py
@@ -2,6 +2,16 @@
 
 name = input("Whats your name?").capitalize().strip()
 
+def errorfixer(number):
+    while True:
+        try:
+            new_num = int(number)
+            break
+        except ValueError:
+            print("Error!")
+            number = input("Enter a proper, whole number.").strip()
+    return new_num
+
 def again():
     again = input("Would you like to make another dinner list?")
     if again == "y":
@@ -12,7 +22,7 @@ def again():
 
 # How many guests would you like to invite
 def dinnerlistmaker():
-    numofguest = int(input("How many guests would you like to invite to the Dinner?".strip()))
+    numofguest = errorfixer(input("How many guests would you like to invite to the Dinner?".strip()))
     # Take user inputed names, add to list, loop untill finished
     guests = []
     for x in range(numofguest):
@@ -24,18 +34,22 @@ def dinnerlistmaker():
         print(f"You are invited to {name}'s dinner {x}")
 
     # Ask to replace someone
-    choice = input("Would you like to remove someone from the invitaion list?")
+    while True:
+        choice = input("Would you like to remove someone from the invitaion list? [y] or [n]: ").strip()
 
-    if choice == "y":
-        numofremove = int(input("How many people would you like to remove: ").strip())
-        for x in range(numofremove):
-            guests.remove(input("Who would you like to remove?").strip().capitalize())
-        # Regenerate invitaions
-        for x in guests:
-            print(f"You are invited to {name}'s dinner {x}")
+        if choice == "y":
+            numofremove = int(input("How many people would you like to remove: ").strip())
+            for x in range(numofremove):
+                guests.remove(input("Who would you like to remove?").strip().capitalize())
+            # Regenerate invitaions
+            for x in guests:
+                print(f"You are invited to {name}'s dinner {x}")
 
-    if choice == "n":
-        print("Then your list is finished!")
+        if choice == "n":
+            print("Then your list is finished!")
+            break
+        else:
+            print("enter only [y] or [n]")
 
     again()
 
