3d5d1e16971498b8e9fcc34a848535c9808a4cc7
mchahal1 <manveerc077@deltalearns.ca>
2025-02-26 18:07:14 +0000

    completed main code, need to do error handiling

--- main.py
+++ main.py
@@ -1,19 +1,45 @@
 #TASK: Dinner Guest List
 
 name = input("Whats your name?").capitalize().strip()
-# How many guests would you like to invite
-numofguest = int(input("How many guests would you like to invite to the Dinner?".strip()))
-# Take user inputed names, add to list, loop untill finished
-guests = []
-for x in range(numofguest):
-    guests.append(input("Enter a name of your guest: ").strip().capitalize())
-    print("Name added!")
 
-# Print invitaions
-for x in guests:
-    print(f"You are invited to {name}'s dinner {x}")
-# Ask to replace someone
-choice = input("Would you like to remove someone from the invitaion list?")
-if choice == "y":
-    guests.remove(input("Who would you like to remove?"))
-# Regenerate invitaions
+def again():
+    again = input("Would you like to make another dinner list?")
+    if again == "y":
+        dinnerlistmaker()
+    if again == "n":
+        print("Goodbye!")
+    return
+
+# How many guests would you like to invite
+def dinnerlistmaker():
+    numofguest = int(input("How many guests would you like to invite to the Dinner?".strip()))
+    # Take user inputed names, add to list, loop untill finished
+    guests = []
+    for x in range(numofguest):
+        guests.append(input("Enter a name of your guest: ").strip().capitalize())
+        print("Name added!")
+
+    # Print invitaions
+    for x in guests:
+        print(f"You are invited to {name}'s dinner {x}")
+
+    # Ask to replace someone
+    choice = input("Would you like to remove someone from the invitaion list?")
+
+    if choice == "y":
+        numofremove = int(input("How many people would you like to remove: ").strip())
+        for x in range(numofremove):
+            guests.remove(input("Who would you like to remove?").strip().capitalize())
+        # Regenerate invitaions
+        for x in guests:
+            print(f"You are invited to {name}'s dinner {x}")
+
+    if choice == "n":
+        print("Then your list is finished!")
+
+    again()
+
+dinnerlistmaker()
+
+    
+
