6f6e3afecfc06ca58fdf9dc8a76c3407ecc962b9
mchahal1 <manveerc077@deltalearns.ca>
2025-02-27 21:30:24 +0000

    fixed the error handilin

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 name = input("Whats your name?").capitalize().strip()
 
-def errorfixer(number):
+def errorfixer(number): # handles any str into int errors
     while True:
         try:
             new_num = int(number)
@@ -12,23 +12,25 @@ def errorfixer(number):
             number = input("Enter a proper, whole number.").strip()
     return new_num
 
-def again():
-    again = input("Would you like to make another dinner list?")
+def again(): # asks to play again
+    again = input("Would you like to make another dinner list? [y] or [n]")
     if again == "y":
         dinnerlistmaker()
     if again == "n":
         print("Goodbye!")
     return
 
-# How many guests would you like to invite
-def dinnerlistmaker():
+
+def dinnerlistmaker(): # Main program
+    # How many guests would you like to invite
     numofguest = errorfixer(input("How many guests would you like to invite to the Dinner?".strip()))
     # Take user inputed names, add to list, loop untill finished
     guests = []
     for x in range(numofguest):
-        guests.append(input("Enter a name of your guest: ").strip().capitalize())
+        guests.append(input("Enter a name of your guest: ").capitalize())
         print("Name added!")
 
+    guests = sorted(guests)
     # Print invitaions
     for x in guests:
         print(f"You are invited to {name}'s dinner {x}")
@@ -36,19 +38,22 @@ def dinnerlistmaker():
     # Ask to replace someone
     while True:
         choice = input("Would you like to remove someone from the invitaion list? [y] or [n]: ").strip()
-
+        
         if choice == "y":
             while True:
                 numofremove = errorfixer(input("How many people would you like to remove: ").strip())
-                for x in range(numofremove):
-                    try:
-                        guests.remove(input("Who would you like to remove?").strip().capitalize())
-                    except ValueError:
+                for x in range(numofremove): # loops however much the user entered 
+                    removedname = input("Who would you like to remove? ").strip().capitalize()
+                    if removedname in guests: # checks if the removed name is in the guest list
+                        guests.remove(removedname) 
+                        print(f"{removedname} has been removed from the list!")
+                    else:
                         print("They are not in the dinner list!")
-                        
+                        break
                 break
                     
             # Regenerate invitaions
+            print("Here is your dinner list:")
             for x in guests:
                 print(f"You are invited to {name}'s dinner {x}")
 
